ce604be0bca8dc4d100d53fcd7d2a4c3bfa8f650
skippson <114942723+skippson@users.noreply.github.com>
2022-10-18 15:47:06 +0300

    Create 1744c.cpp

---
+++ lapin_i_o/1744c.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+#include<string>
+
+int main(){
+    int t;
+    std::cin >> t;
+    for(int iT = 0; iT < t; iT++){
+        int n;
+        std::cin >> n;
+        std::string c;
+        getline(std::cin, c);
+        for(int i = 0; i < n; i++){
+            std::string s;
+            getline(std::cin, s);
+            
+        }
+
+
+    }
+    return 0;
+}
